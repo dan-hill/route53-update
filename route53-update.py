c4b43772fc1ebad9cb5e3263f273104972e21143
Dan Hill <dan@linux-dev.(none)>
2013-11-03 21:15:31 -0600

    no longer querys aws every check

--- route53-update.py
+++ route53-update.py
@@ -4,41 +4,75 @@ import boto
 import json
 from urllib2 import urlopen
 import configparser
+import time
 
 # Uncomment the following line for debugging
-boto.set_stream_logger('boto')
+# boto.set_stream_logger('boto')
+
 
 def monitor(route53, host_id, host_name, check_interval):
+    print 'Monitoring...'
+    # Initial check and update
+    if external_ip() != zone_ip():
+        update(external_ip())
+    
+    host_ip = external_ip()
+
     while True:
-        # Request record set from amazon
-        records = route53.get_all_rrsets(host_id, 'A', host_name, 1)
-        records = [record for record in records if record.name.startswith(host_name)]
-    
-        # Get DNS IP from record set
-        dns_ip = records[0].resource_records[0]
-    
-        # Get external IP
-        host_ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
+  
+        # Check if external ip has changed
+        if host_ip != external_ip():
 
-        # Compare IPs and update if nessisary
-        if dns_ip != host_ip:
-            update(host_ip)
-        else:
-            print "DNS is OK."
+            # Compare IPs and update if nessisary
+            if external_ip() != zone_ip() :
+                update(external_ip())
+          
+        # Sleep until next check
+        time.sleep(check_interval)
 
-    time.sleep(check_interval)
+
 
 def update(host_ip):
+
+    # Get the current record set
+    records = request_record_set()
+
+    # Set up the record set with new data
     change = boto.route53.record.ResourceRecordSets(route53, host_id, 'IP updated')
-    if record:
-        change.add_change_record("DELETE", records[0])
-        change.add_change_record("CREATE", boto.route53.record.Record(name = host_name,
+    
+    # Delete the old record set from Route 53
+    change.add_change_record("DELETE", records[0])
+    
+    # Save the updated record set to Route 53
+    change.add_change_record("CREATE", boto.route53.record.Record(name = host_name,
                                                                       type = "A",
                                                                       resource_records = [host_ip],
                                                                       alias_hosted_zone_id = host_id))
-        change.commit()
-    print "IP was updated on DNS"
+    # Commit all record changes
+    change.commit()
 
+
+
+def external_ip():
+    return json.load(urlopen('http://httpbin.org/ip'))['origin']
+
+
+
+def zone_ip():
+    records = request_record_set()
+    
+    # Return DNS IP from record set
+    return records[0].resource_records[0]
+
+
+
+def request_record_set():
+    # Request record set from amazon
+    records = route53.get_all_rrsets(host_id, 'A', host_name, 1)
+    return [record for record in records if record.name.startswith(host_name)]
+
+
+     
 if __name__ == "__main__":
 
     # Load configuration file
@@ -50,7 +84,7 @@ if __name__ == "__main__":
     access_key     = config['authentication']['access_key']
     host_id        = config['zone']['host_id']
     host_name      = config['zone']['host_name']
-    check_interval = config['monitor']['check_interval']
+    check_interval = float( config['monitor']['check_interval'])
 
     # Connect to Route 53
     route53 = boto.route53.connection.Route53Connection(aws_access_key_id = access_key,
